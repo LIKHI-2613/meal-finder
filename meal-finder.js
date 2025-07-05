202938b112d112d89d39f80d5d197d6589d01e56
LIKHI-2613 <likhithayellama@gmail.com>
2025-07-05 10:21:55 +0530

    Add title attribute to Home link for improved accessibility

--- meal-finder.js
+++ meal-finder.js
@@ -392,7 +392,7 @@ console.log('Meal Finder JS loaded!');
                 <!-- Breadcrumb: Home symbol and meal name in one line, orange background, slightly larger font -->
                 <div class="bg-orange-500 text-white py-3 px-4 rounded-lg shadow-md mb-4">
                     <div class="text-base md:text-lg flex items-center">
-                        <a href="#" onclick="showCategoriesView(event)" class="hover:underline flex items-center inline-block">
+                        <a href="#" onclick="showCategoriesView(event)" class="hover:underline flex items-center inline-block" title="Back to Home">
                             <svg class="w-5 h-5 inline-block align-text-bottom" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg> 
                         </a> 
                         <span class="mx-2 font-bold">>></span> 
